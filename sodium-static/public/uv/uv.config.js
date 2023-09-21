ede4ab029a4d122a7895a4aee28a121f67c52c24
tannerjustaguy <145362974+tannerjustaguy@users.noreply.github.com>
2023-09-21 07:26:38 -0400

    uv.config.js

--- sodium-static/public/uv/uv.config.js
+++ sodium-static/public/uv/uv.config.js
@@ -1,7 +1,7 @@
 self.__uv$config = {
   prefix: "/uv/service/",
   bare: [
-    "https://tomp.app",
+    /bare/",
   ],
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
